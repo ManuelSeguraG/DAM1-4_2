090f68cf7b2ebd7be363249dc6d77b30e1d98413
ManuelSeguraG <mjsegura@iesrafaelalberti.es>
2021-11-24 20:35:13 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,52 @@
+package un4.eje4_2
+
+// TIENDA
+data class Tienda(val nombre: String, val clientes: List<Clientes>) {
+    fun obtenerConjuntoDeClientes(): Set<Clientes> = clientes.toSet()
+
+    fun obtenerCiudadesDeClientes(): Set<Ciudad> = clientes.map { it.ciudad }.toSet()
+
+    fun obtenerClientesPor(ciudad:Ciudad): List<Clientes> = clientes.filter { it.ciudad == ciudad }
+
+    fun checkTodosClientesSonDe(ciudad : Ciudad): Boolean = clientes.all { it.ciudad == ciudad }
+
+    fun hayClientesDe(ciudad: Ciudad): Boolean = clientes.any { it.ciudad == ciudad }
+
+    fun cuentaClientesDe(ciudad: Ciudad): Int = clientes.count { it.ciudad == ciudad }
+
+    fun encuentraClienteDe(ciudad: Ciudad): Clientes? = clientes.find { it.ciudad == ciudad }
+
+    fun obtenerClientesOrdenadosPorPedidos(): List<Clientes> = clientes.sortedByDescending { it.pedidos.size }
+
+    fun obtenerClientesConPedidosSinEntregar(): Set<Clientes> = TODO()
+
+}
+
+// CLIENTES
+data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
+    override fun toString() = "$nombre from ${ciudad.nombre}"
+
+    fun obtenerProductosPedidos(): List<Producto> = TODO()
+
+}
+
+// PEDIDO
+data class Pedido(val productos: List<Producto>, val estaEntregado: Boolean)
+
+// PRODUCTO
+data class Producto(val nombre: String, val precio: Double) {
+    override fun toString() = "'$nombre' for $precio"
+}
+
+// CIUDAD
+data class Ciudad(val nombre: String) {
+    override fun toString() = nombre
+}
+
+// MAIN
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
